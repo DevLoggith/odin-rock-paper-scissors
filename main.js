d16ca5abd65dc333f05686e21ae5ca6658d48f41
Logan <Logmiklos@gmail.com>
2021-12-01 15:30:42 -0500

    Create .html, .js, and .txt files.   *Also create first few variables and functions in .js

---
+++ main.js
@@ -0,0 +1,16 @@
+const choices = ['Rock', 'Paper', 'Scissors']
+const computerSelection = choices[Math.floor(Math.random() * choices.length)];
+const playerSelection = ''; 
+const restart = '';
+
+function computerPlay() {
+
+}
+
+function playRound(computerSelection, playerSelection) {
+
+}
+
+function game() {
+
+}
