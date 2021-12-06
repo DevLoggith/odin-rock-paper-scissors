86935b973c4ac22bc50c13bd5f2da5356efb6d84
Logan <Logmiklos@gmail.com>
2021-12-06 16:11:34 -0500

    Add elements to make console read out more verbose

--- main.js
+++ main.js
@@ -1,48 +1,101 @@
 const choices = ['rock', 'paper', 'scissors'];
 let playerSelection;
-const computerSelection = computerPlay()
+let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
+let tie = 0;
 
 function computerPlay() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = window.prompt( "Select either Rock, Paper, or Scissors" ).toLowerCase();
+  playerSelection = window.prompt( "Select either Rock, Paper, or Scissors").toLowerCase();
+  computerSelection = computerPlay();
 
-  if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    playerScore++;
-    return "You win! Rock beats Scissors";
-  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    playerScore++;
-    return "You win! Paper beats Rock";
-  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    playerScore++;
-    return "You win! Scissors beats Paper";
-  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-    computerScore++;
-    return "You lose, paper beats Rock";
-  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-    computerScore++;
-    return "You lose, Scissors beats Paper";
-  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-    computerScore++;
-    return "You lose, Rock beats Scissors";
-  } else { 
-    return "It's a tie!";
+  console.log(playerSelection)
+  console.log(computerSelection)
+  
+  switch (true) {
+  
+    case (playerSelection === 'rock' && computerSelection === 'scissors'):
+      playerScore++;
+      console.log("You win! Rock beats Scissors");
+      console.log(playerScore, "<-- Your Score");
+      console.log(computerScore, "<-- Computer's Score");
+      console.log(tie, "<-- Ties");
+      break;
+      
+    case (playerSelection === 'paper' && computerSelection === 'rock'):
+      playerScore++;
+      console.log("You win! Paper beats Rock");
+      console.log(playerScore, "<-- Your Score");
+      console.log(computerScore, "<-- Computer's Score");
+      console.log(tie, "<-- Ties");
+      break;
+      
+    case (playerSelection === 'scissors' && computerSelection === 'paper'):
+      playerScore++;
+      console.log("You win! Scissors beats Paper");
+      console.log(playerScore, "<-- Your Score");
+      console.log(computerScore, "<-- Computer's Score");
+      console.log(tie, "<-- Ties");
+      break;
+      
+    case (playerSelection === 'rock' && computerSelection === 'paper'):
+      computerScore++;
+      console.log("You lose, paper beats Rock");
+      console.log(playerScore, "<-- Your Score");
+      console.log(computerScore, "<-- Computer's Score");
+      console.log(tie, "<-- Ties");
+      break;
+      
+    case (playerSelection === 'paper' && computerSelection === 'scissors'):
+      computerScore++;
+      console.log("You lose, Scissors beats Paper");
+      console.log(playerScore, "<-- Your Score");
+      console.log(computerScore, "<-- Computer's Score");
+      console.log(tie, "<-- Ties");
+      break;
+      
+    case (playerSelection === 'scissors' && computerSelection === 'rock'):
+      computerScore++;
+      console.log("You lose, Rock beats Scissors");
+      console.log(playerScore, "<-- Your Score");
+      console.log(computerScore, "<-- Computer's Score");
+      console.log(tie, "<-- Ties");
+      break;
+      
+    case (playerSelection === computerSelection):
+      tie++;
+      console.log("It's a tie!");
+      console.log(playerScore, "<-- Your Score");
+      console.log(computerScore, "<-- Computer's Score");
+      console.log(tie, "<-- Ties");
+      break;
+      
+    default:
+      console.log("Please enter a valid choice");
   }
 }
 
 function game() {
   
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+  if (playerScore > computerScore) {
+    return "Congrats, You won!";
+  } else if (playerScore < computerScore) {
+    return "Game over, better luck next time";
+  } else {
+    return "Tie game, try again!";
+  }
 }
 
 console.log(game());
-console.log(playerScore);
-console.log(computerScore);
+console.log(playerScore, "<-- Your Score");
+console.log(computerScore, "<-- Computer's Score");
+console.log(tie, "<-- Ties");
